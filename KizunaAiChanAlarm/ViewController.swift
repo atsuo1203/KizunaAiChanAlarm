d827281d049adcb96357e34e6f6c1b01416a0b42
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-25 03:36:01 +0900

    Initial Commit

---
+++ KizunaAiChanAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KizunaAiChanAlarm
+//
+//  Created by Atsuo Yonehara on 2017/08/25.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
