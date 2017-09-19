e3e5a4d3d52c2c1f083abbee41526713e1f48dd1
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-19 11:50:33 +0900

    tabbarを各storyboardに写した

--- KizunaAiChanAlarm/MainTabController.swift
+++ KizunaAiChanAlarm/MainTabController.swift
@@ -16,17 +16,14 @@ class MainTabController: UITabBarController {
         
         let firstStoryboard = UIStoryboard(name: "Alarm", bundle: nil)
         let firstViewController = firstStoryboard.instantiateInitialViewController()
-        firstViewController?.tabBarItem = UITabBarItem(title: "Alarm", image: UIImage(named: "clock_32.png"), tag: 1)
         viewControllers.append(firstViewController!)
         
         let secondStoryboard = UIStoryboard(name: "AIChannel", bundle: nil)
         let secondViewController = secondStoryboard.instantiateInitialViewController()
-        secondViewController?.tabBarItem = UITabBarItem(title: "A.I.chanel", image: UIImage(named: "youtube_32.png"), tag: 2)
         viewControllers.append(secondViewController!)
         
         let thirdStoryboard = UIStoryboard(name: "Website", bundle: nil)
         let thirdViewController = thirdStoryboard.instantiateInitialViewController()
-        thirdViewController?.tabBarItem = UITabBarItem(title: "Website", image: UIImage(named: "pc_32.png"), tag: 3)
         viewControllers.append(thirdViewController!)
         
         self.setViewControllers(viewControllers, animated: false)
