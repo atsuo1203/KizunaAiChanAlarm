3b41c7d3451f3e222bea2357418bddf5ddf0b5b4
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-19 19:02:44 +0900

    web表示

--- KizunaAiChanAlarm/AIChannelViewController.swift
+++ KizunaAiChanAlarm/AIChannelViewController.swift
@@ -11,15 +11,23 @@ import UIKit
 class AIChannelViewController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
+    let targetURL = "https://www.youtube.com/channel/UC4YaOt1yT-ZeyB0OmxHgolA/feed?activity_view=1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        loadAddressURL()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadAddressURL(){
+        guard let requestURL = URL(string: targetURL) else { return }
+        let request = URLRequest(url: requestURL)
+        webView.loadRequest(request)
     }
     
 
