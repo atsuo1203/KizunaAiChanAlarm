1a570bcc04a828226b572573a12e07612a3480ae
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-21 01:23:12 +0900

    webView実装

--- KizunaAiChanAlarm/MainTabController.swift
+++ KizunaAiChanAlarm/MainTabController.swift
@@ -15,25 +15,13 @@ class MainTabController: UITabBarController {
         super.viewDidLoad()
         var viewControllers: [UIViewController] = []
         
-        let firstStoryboard = UIStoryboard(name: "Alarm", bundle: nil)
-        let firstViewController = firstStoryboard.instantiateInitialViewController()
-        viewControllers.append(firstViewController!)
+        let alarmStoryboard = UIStoryboard(name: "Alarm", bundle: nil)
+        let alarmViewController = alarmStoryboard.instantiateInitialViewController()
+        viewControllers.append(alarmViewController!)
         
-        let secondStoryboard = UIStoryboard(name: "AIChannel", bundle: nil)
-        let secondViewController = secondStoryboard.instantiateInitialViewController()
-        viewControllers.append(secondViewController!)
-        
-        let thirdStoryboard = UIStoryboard(name: "Website", bundle: nil)
-        let thirdViewController = thirdStoryboard.instantiateInitialViewController()
-        viewControllers.append(thirdViewController!)
-
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .red
-        let button = UIButton(frame: CGRect(x: 100, y: 200, width: 200, height: 50))
-        button.setTitle("Button", for: .normal)
-        button.addTarget(self, action: #selector(showWebView), for: .touchUpInside)
-        viewController.view.addSubview(button)
-        viewControllers.append(viewController)
+        let webStoryboard = UIStoryboard(name: "Website", bundle: nil)
+        let webViewController = webStoryboard.instantiateInitialViewController()
+        viewControllers.append(webViewController!)
         
         self.setViewControllers(viewControllers, animated: false)
         
--- KizunaAiChanAlarm/WebsiteViewController.swift
+++ KizunaAiChanAlarm/WebsiteViewController.swift
@@ -7,21 +7,23 @@
 //
 
 import UIKit
+import SafariServices
 
 class WebsiteViewController: UIViewController {
-
-    @IBOutlet weak var webView: UIWebView!
-    @IBOutlet weak var backImageView: UIImageView!
-    @IBOutlet weak var fowardImageView: UIImageView!
-    @IBOutlet weak var reloadImageView: UIImageView!
     
-    let targetURL = "https://kizunaai.com"
+    @IBAction func aichannelButtonTapped(_ sender: UIButton) {
+        showWebView(targetURL: "https://www.youtube.com/channel/UC4YaOt1yT-ZeyB0OmxHgolA/feed?activity_view=1")
+    }
+    @IBAction func aiGamesButtonTapped(_ sender: UIButton) {
+        showWebView(targetURL: "https://www.youtube.com/channel/UCbFwe3COkDrbNsbMyGNCsDg/feed?activity_view=1")
+    }
+    @IBAction func websiteButtonTapped(_ sender: UIButton) {
+        showWebView(targetURL: "https://kizunaai.com/")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupSwipeGestures()
-        loadAddressURL()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,44 +31,12 @@ class WebsiteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func loadAddressURL(){
-        guard let requestURL = URL(string: targetURL) else { return }
-        let request = URLRequest(url: requestURL)
-        webView.loadRequest(request)
+    func showWebView(targetURL: String) {
+        let url = URL(string: targetURL)!
+        let webView = SFSafariViewController(url: url)
+        present(webView, animated: true, completion: nil)
     }
-    
-    func setupSwipeGestures() {
-        //右ボタンにジェスチャー
-        let rightButtonGesture = UITapGestureRecognizer(target: self, action: #selector(self.goForward))
-        fowardImageView.isUserInteractionEnabled = true
-        fowardImageView.addGestureRecognizer(rightButtonGesture)
-        
-        //左ボタンにジェスチャー
-        let leftButtonGesture = UITapGestureRecognizer(target: self, action: #selector(self.goBack))
-        backImageView.isUserInteractionEnabled = true
-        backImageView.addGestureRecognizer(leftButtonGesture)
-        
-        //リロードボタンにジェスチャー
-        let reloadButtonGesture = UITapGestureRecognizer(target: self, action: #selector(self.reloadRequest))
-        reloadImageView.isUserInteractionEnabled = true
-        reloadImageView.addGestureRecognizer(reloadButtonGesture)
-        
-    }
-    
-    func goBack() {
-        if webView.canGoBack {
-            webView.goBack()
-        }
-    }
-    
-    func goForward() {
-        if webView.canGoForward {
-            webView.goForward()
-        }
-    }
-    
-    func reloadRequest(){
-    }
+
 
     /*
     // MARK: - Navigation
